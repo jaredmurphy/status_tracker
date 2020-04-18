41694bfe478f99a229c71e2d37a2c720516f3e08
Jared Murphy <jaredcmurphy@gmail.com>
2020-04-18 15:03:47 -0400

    remove devise

--- Gemfile
+++ Gemfile
@@ -28,8 +28,6 @@ gem 'jbuilder', '~> 2.7'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
 
-gem 'devise'
-
 group :development, :test do
   gem 'pry'
   gem 'factory_bot_rails'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,6 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-
   validates :username, presence: true
+  # email
 
   has_many :follower_requests, class_name: "FollowedUser", foreign_key: :following_id, dependent: :destroy
   has_many :following_requests, class_name: "FollowedUser", foreign_key: :follower_id, dependent: :destroy
