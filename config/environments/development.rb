dc59d9bacd88a0d1028f121ddf6d43691caa58aa
Jared Murphy <jaredcmurphy@gmail.com>
2020-04-19 14:27:02 -0400

    install stimulus & stimulus reflex

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,8 @@
 Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.session_store :cache_store
+
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
 
